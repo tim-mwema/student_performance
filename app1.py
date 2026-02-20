f0a00ee25140e3ab89d3b2865d00b533d16385c4
tim-mwema <mwemalewis35@gmail.com>
2026-02-20 16:03:39 +0300

    updated

--- app1.py
+++ app1.py
@@ -1,10 +1,5 @@
 import pandas as pd 
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,accuracy_score,confusion_matrix
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import GradientBoostingClassifier
-from imblearn.over_sampling import SMOTE
 import joblib
 import streamlit as st
 #loading model
