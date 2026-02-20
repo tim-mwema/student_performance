a10efe279992f7e521156817072c3371e072df29
tim-mwema <mwemalewis35@gmail.com>
2026-02-20 17:19:43 +0300

    GBC Version 10

--- app1.py
+++ app1.py
@@ -8,8 +8,8 @@ scaler=joblib.load("scaler1.pkl")
 #start of streamlit code
 st.title("Student Performance Classifier")
 st.write("<span style='color:#4CAF50'>**Input the details based on Study Hours & Sleep Hours of a single student to classify performance**</span>",unsafe_allow_html=True)
-Study_Hours=st.number_input("**Study time(hrs)**",min_value=0.500000,max_value=24.000000,step=0.100000,format="%.9f")
-Sleep_Hours=st.number_input('**Sleep time(hrs)**',min_value=0.500000,max_value=24.000000,step=0.100000,format="%.9f")
+Study_Hours=st.number_input("**Study  time(hrs)**",min_value=0.500000,max_value=24.000000,step=0.100000,format="%.9f")
+Sleep_Hours=st.number_input('**Sleep  time(hrs)**',min_value=0.500000,max_value=24.000000,step=0.100000,format="%.9f")
 # action button
 if st.button("Classify",type="primary"):
     input_features=np.array([[Study_Hours,Sleep_Hours]])
