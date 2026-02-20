f78a7615e0e5bcc86bbc5a9ebd5cd57654116042
tim-mwema <mwemalewis35@gmail.com>
2026-02-20 20:41:41 +0300

    Improved GBC of 95% accuracy & mild overfitting of 0.05% .Test_score=0.95,train_score=1

--- app1.py
+++ app1.py
@@ -4,21 +4,21 @@ import joblib
 import streamlit as st
 #loading model
 model=joblib.load("model1.pkl")
-scaler=joblib.load("scaler1.pkl")
+scaler1=joblib.load("scaler1.pkl")
 #start of streamlit code
 st.title("Student Performance Classifier")
 st.write("<span style='color:#4CAF50'>**Input the details based on Study Hours & Sleep Hours of a single student to classify performance**</span>",unsafe_allow_html=True)
-Study_Hours=st.number_input("**Study  time(hrs)**",min_value=0.500000,max_value=24.000000,step=0.100000,format="%.9f")
-Sleep_Hours=st.number_input('**Sleep  time(hrs)**',min_value=0.500000,max_value=24.000000,step=0.100000,format="%.9f")
+Study_Hours=st.number_input("**Study  time(hrs)**",min_value=0.5,max_value=24.0,step=0.1,format="%.9f")
+Sleep_Hours=st.number_input('**Sleep  time(hrs)**',min_value=0.5,max_value=24.0,step=0.1,format="%.9f")
 # action button
 if st.button("Classify",type="primary"):
     input_features=np.array([[Study_Hours,Sleep_Hours]])
     #scale feature
-    input_features_scaled=scaler.transform(input_features)
+    input_features_scaled=scaler1.transform(input_features)
     #make prediction now
-    prediction=model.predict(input_features)
+    prediction=model.predict(input_features_scaled)
     #map pass values
-    pas= { 0:'Student Fails',1:'Student Passes'}
-    student_performance=pas[prediction[0]]
+    pas= { 1:'Student Fails',0:'Student Passes'}
+    student_performance=pas[int(prediction[0])]
     #display now
     st.success(f"The predicted performance is: {student_performance}")
